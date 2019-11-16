01bd20da860c75cd77be3f15903b7461c19ce59d
Alexander <gcastilloalex@gmail.com>
2019-11-15 20:56:30 -0500

    Imagen Angular

---
+++ informacion.md
@@ -0,0 +1,16 @@
+Componentes
+```javascript
+ng g c components/navigation
+ng g c components/photo-form
+ng g c components/photo-preview
+ng g c components/photo-list
+ng g s services/photo
+
+//bootswatch
+npm i bootswatch
+
+```
+
+//Github
+git remote add origin https://github.com/AlexanderCastilloG/angular-images.git
+git push -u origin master
---
+++ src/app/components/photo-list/photo-list.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Photo } from '../../interfaces/Photo';
+import { PhotoService } from '../../services/photo.service';
+
+@Component({
+  selector: 'app-photo-list',
+  templateUrl: './photo-list.component.html',
+  styleUrls: ['./photo-list.component.css']
+})
+export class PhotoListComponent implements OnInit {
+
+
+  photos: Photo[] = [];
+
+  constructor(private photoService: PhotoService,
+              private router: Router) { }
+
+  ngOnInit() {
+
+    this.photoService.getPhotos().subscribe(res => {
+      this.photos = res;
+    }, err => {
+      console.log(err);
+    });
+  }
+
+  selectedCard(id: string){
+    this.router.navigate(['/photos', id]);
+  }
+
+}
